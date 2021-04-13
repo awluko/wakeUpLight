3d162977d7e5c3cddd340762a2d777a4eece1ca0
awluko <andrew.w.lukosevic@gmail.com>
2021-04-12 21:55:09 -0500

    Added comments to wakeUpLight.py and updated for this weeks schedule

--- wakeUpLight.py
+++ wakeUpLight.py
@@ -1,15 +1,23 @@
+######################################
+# wakeUpLight.py is a script that runs on a wake up light using a NeoPixel ring and a Raspberry Pi Zero WH
+######################################
+
 import board, neopixel, time
 from datetime import datetime, timedelta
 
+# Sets the pixels variable to the D18 pin on the board and establishes that there are 12 LEDs. These values may need to change
 pixels = neopixel.NeoPixel(board.D18,12)
-earlyDays = ["Mon","Tue","Wed","Fri"]
+# Lists for day of the week, ideally this will become more dynamic in the future, Thursday isn't on the list because it is not a work day currently
+earlyDays = ["Mon","Tue","Fri"]
+earlierDays = ["Wed"]
 weekend = ["Sun", "Sat"]
 
-#thing = "go"
 
+
+# Compares the times set based on the day versus the current time. 
 def timeComp(h, m, mMax, nowHour, nowMin):
-	#global thing
 	print("timeComp has been called")
+	# Displays current settings passed to the function
 	print(f"\nSettings are:\nh: {h}\nm: {m}\nmMax: {mMax}\nnowHour: {nowHour}\nnowMin: {nowMin}\n")
 	try:
 		if nowHour == h:
@@ -25,54 +33,57 @@ def timeComp(h, m, mMax, nowHour, nowMin):
 			else:
 				print("It is too late, waiting to try again")
 				time.sleep(300)
-				print("Done waiting")	
+				print("Done waiting")
 		else:
 			print("The hour is wrong, sleeping for 5 minutes\n")
 			time.sleep(300)
 	except Exception as e:
 		print(f"There was an error.\nThe error was {e}")
 
+# Turns off the LEDs
 def lightsOff():
 	pixels.fill((0,0,0))
 
+# Function that gradually increases the brightness of the LEds
 def goodMorning():
-	for x in range(5,256,5):
-		#print(f"x is {x}")
+	# Goes from 5 to 256 and sets the color and brightness of the LEDs
+	for x in range(5,256,3):
+		# Sets the LEDs sequentially
 		for y in range(12):
-			#print(f"y is {y}")
 			pixels[y] = (x,x,0)
 			time.sleep(0.1)
 
+# Turns the LEDs from a bright blue down until the LEDs are off
 def goodNight():
-	for x in range(5,256,5):
+	for x in range(5,256,3):
 		brightness = 256 - x
-		#print(f"Brightness is {brightness}")
 		for y in range(12):
 			pixels[y]=(0,0,brightness)
 			time.sleep(0.5)
-			#print(f"y is {y}")
-
-
-#goodMorning()
-#goodNight()
-#goodMorning()
-#lightsOff()
 
 def main():
-	while True: # thing != "stop":		
+	while True: # Keeps the loop open		
+		# Pull the current time
 		now = datetime.now()
-		nowDay = str(now.strftime("%a"))
-		nowHour = int(now.strftime("%H"))
-		nowMin = int(now.strftime("%M"))
+		nowDay = str(now.strftime("%a")) # Day of the week, shorthand (e.g. Mon)
+		nowHour = int(now.strftime("%H")) # Hour of the day, 24 hour clock
+		nowMin = int(now.strftime("%M")) # Minute of the hour
 		print(f"It is {nowDay}, at {nowHour}:{nowMin}\n")
 
 		if nowDay in earlyDays:
 			print("earlyDays\n")
-			timeComp(6,00,20,nowHour,nowMin)
+			# Will prompt the goodMorning function to go off when between 0612 and 0625
+			timeComp(6,12,25,nowHour,nowMin)
+		elif nowDay in earlierDays:
+			print("earlierDays\n")
+			# Prompts goodMorning() to go off between 0600 and 0615
+			timeComp(6,0,15,nowHour,nowMin)
 		elif nowDay in weekend:
+			# Prompts goodMorning() to go off between 0900 and 0930
 			timeComp(9,30,45,nowHour,nowMin)
 		else:
 			print("Today is Thursday")
+			# Prompts goodMorning() to go off between 1030 and 1045
 			timeComp(10,30,45,nowHour,nowMin)
 
 main()
